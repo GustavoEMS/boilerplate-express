c0298bd39e185fb1f4d0e0ca0fd1600cd8dff07f
Gustavo Meza <gokudiosxd@gmail.com>
2025-05-04 21:48:42 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res) {
-    
+    res.send('Hello Express');
   });
 
-  console.log('Hello Express'); // Se ve en el navegador
+  
 
 
 
